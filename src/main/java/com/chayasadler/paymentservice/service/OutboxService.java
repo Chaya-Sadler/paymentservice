34662f54e8dd04c789fc83192ebbd6243f36bfbd
chaya <chaya.r.sadler@gmail.com>
2025-11-20 12:06:08 -0600

    code refactoring

--- src/main/java/com/chayasadler/paymentservice/service/OutboxService.java
+++ src/main/java/com/chayasadler/paymentservice/service/OutboxService.java
@@ -1,30 +1,55 @@
 package com.chayasadler.paymentservice.service;
 
-import com.chayasadler.paymentservice.dao.IEventRepository;
+import com.chayasadler.paymentservice.dao.IOutBoxRepository;
 import com.chayasadler.paymentservice.model.OutBoxEvent;
 import com.chayasadler.paymentservice.util.EventStatus;
+import com.chayasadler.paymentservice.util.InventoryEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 public class OutboxService {
 
     @Autowired
-    IEventRepository iEventRepository;
+    IOutBoxRepository iOutBoxRepository;
 
-    public void saveEvent(String eventType, String orderId, String payload) {
+    @Autowired
+    ObjectMapper mapper;
+
+    public void saveEvent(String eventType, InventoryEvent inventoryEvent, String paymentStatus) {
+        String payload;
+        InventoryEvent paymentEvent = new InventoryEvent(
+                UUID.randomUUID(), //new unique message id for idempotency of kafka messages
+                eventType,
+                inventoryEvent.orderId(),
+                inventoryEvent.customerId(),
+                inventoryEvent.totalAmt(),
+                inventoryEvent.orderItemEventList(),
+                paymentStatus.equals("AUTHORIZED") ? "Completed" : "Cancelled", //order status
+                paymentStatus,
+                LocalDateTime.now()
+        );
+        String paymentPayload;
+        try {
+            paymentPayload = mapper.writeValueAsString(paymentEvent);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
 
         OutBoxEvent outBoxEvent = new OutBoxEvent();
 
         outBoxEvent.setEventType(eventType);
-        outBoxEvent.setAggregateId(orderId);
+        outBoxEvent.setAggregateId(inventoryEvent.orderId().toString());
         outBoxEvent.setAggregateType("payment");
-        outBoxEvent.setPayload(payload);
+        outBoxEvent.setPayload(paymentPayload);
         outBoxEvent.setStatus(EventStatus.UNSENT.name());
         outBoxEvent.setCreatedAt(LocalDateTime.now());
 
-        iEventRepository.save(outBoxEvent);
+        iOutBoxRepository.save(outBoxEvent);
     }
 }
